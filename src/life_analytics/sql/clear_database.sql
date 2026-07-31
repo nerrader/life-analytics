32cbcdaad01879c729612cb75eabfe74b6643a0f
nerrader <nerrader@proton.me>
2026-07-31 20:58:12 +0700

    chore: add transaction and add many missing semicolons

--- src/life_analytics/sql/clear_database.sql
+++ src/life_analytics/sql/clear_database.sql
@@ -1,4 +1,8 @@
-DELETE FROM daily_summaries
-DELETE FROM activities
-DELETE FROM sleep
+BEGIN TRANSACTION;
+
+DELETE FROM daily_summaries;
+DELETE FROM activities;
+DELETE FROM sleep;
 DELETE FROM sqlite_sequence;
+
+COMMIT;
