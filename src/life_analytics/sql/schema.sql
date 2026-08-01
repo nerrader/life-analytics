d307c95661627e7e362b280e87cae527bc3aebba
nerrader <nerrader@proton.me>
2026-08-01 08:52:51 +0700

    docs: change unix timestamp to iso string, refactor: remove productivity column in activities table

--- src/life_analytics/sql/schema.sql
+++ src/life_analytics/sql/schema.sql
@@ -1,5 +1,5 @@
 CREATE TABLE IF NOT EXISTS daily_summaries (
-    date TEXT PRIMARY KEY, --unix timestamp
+    date TEXT PRIMARY KEY, --iso string
     mood INTEGER NOT NULL CHECK(mood BETWEEN 1 AND 10),
     productivity INTEGER NOT NULL CHECK(productivity BETWEEN 1 AND 10),
     stress INTEGER NOT NULL CHECK(stress BETWEEN 1 AND 10)
@@ -9,18 +9,17 @@ CREATE TABLE IF NOT EXISTS activities (
     activity_id INTEGER PRIMARY KEY AUTOINCREMENT,
     date TEXT NOT NULL,
     activity TEXT NOT NULL,
-    activity_start TEXT NOT NULL, --unix timestamp
-    activity_end TEXT NOT NULL, --unix timestamp
+    activity_start TEXT NOT NULL, --iso string
+    activity_end TEXT NOT NULL, --iso string
     difficulty INTEGER NOT NULL CHECK(difficulty BETWEEN 1 AND 10),
     enjoyability INTEGER NOT NULL CHECK(enjoyability  BETWEEN 1 AND 10),
-    productivity INTEGER NOT NULL CHECK(productivity BETWEEN 1 AND 10),
 
     FOREIGN KEY(date) REFERENCES daily_summaries(date)
 );
 
 CREATE TABLE IF NOT EXISTS sleep (
     sleep_id INTEGER PRIMARY KEY AUTOINCREMENT,
-    sleep_start_time TEXT NOT NULL, --unix timestamp
-    sleep_end_time TEXT NOT NULL, --unix timestamp
+    sleep_start_time TEXT NOT NULL, --iso string
+    sleep_end_time TEXT NOT NULL, --iso string
     mood INTEGER NOT NULL CHECK(mood BETWEEN 1 AND 10)
 );
