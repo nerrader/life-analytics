83146ecebf1bbf62e71a52e800f58c65c706e3e2
nerrader <nerrader@proton.me>
2026-09-15 19:14:10 +0700

    feat: add normalizing datetime in migrations script

--- src/life_analytics/logic/migrations.py
+++ src/life_analytics/logic/migrations.py
@@ -14,5 +14,53 @@ def migrate_database(database_path: Path) -> None:
             if schema_version == 0:
                 print("New schema version, upgrading database from v0 to v1.")
                 connection.executescript((MIGRATIONS_DIR / "v0_to_v1.sql").read_text())
+            update_datetimes(connection)
     finally:
         connection.close()
+
+
+def update_datetimes(connection: sqlite3.Connection) -> None:
+    def normalize_datetime(isostring: str) -> str:
+        """This makes sure the time part of the datetime isostring is valid.
+        Example: 2026-12-25T6:30 -> 2026-12-25T06:30"""
+        date, time = isostring.split("T", 1)
+        hour, minutes = time.split(":", 1)
+
+        return f"{date}T{hour.zfill(2)}:{minutes}"
+
+    activities = connection.execute(
+        "SELECT id, start_at, end_at FROM activities"
+    ).fetchall()
+
+    for activity_id, start_at, end_at in activities:
+        connection.execute(
+            """
+            UPDATE activities
+            SET start_at = ?, end_at = ?
+            WHERE id = ?
+            """,
+            (
+                normalize_datetime(start_at),
+                normalize_datetime(end_at),
+                activity_id,
+            ),
+        )
+
+    # Sleep
+    sleep_records = connection.execute(
+        "SELECT id, start_at, end_at FROM sleep"
+    ).fetchall()
+
+    for sleep_id, start_at, end_at in sleep_records:
+        connection.execute(
+            """
+            UPDATE sleep
+            SET start_at = ?, end_at = ?
+            WHERE id = ?
+            """,
+            (
+                normalize_datetime(start_at),
+                normalize_datetime(end_at),
+                sleep_id,
+            ),
+        )
