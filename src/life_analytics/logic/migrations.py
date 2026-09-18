a36d2f3f12c744ba870e64d9c7382e7e34ff7255
nerrader <nerrader@proton.me>
2026-09-18 18:22:11 +0700

    feat: migrations make backups now

--- src/life_analytics/logic/migrations.py
+++ src/life_analytics/logic/migrations.py
@@ -1,7 +1,9 @@
 import sqlite3
+from datetime import datetime
 from importlib.resources import files
 from importlib.resources.abc import Traversable
 from pathlib import Path
+from shutil import copy2
 
 from life_analytics.utils.time_utils import normalize_datetime
 
@@ -15,10 +17,17 @@ def migrate_database(database_path: Path) -> None:
             schema_version = connection.execute("PRAGMA user_version;").fetchone()[0]
             if schema_version == 0:
                 print("New schema version, upgrading database from v0 to v1.")
+                make_backup(database_path)
+                print(f"Made a backup at {database_path}.backup if something breaks.")
                 connection.executescript((MIGRATIONS_DIR / "v0_to_v1.sql").read_text())
             update_datetimes(connection)
     finally:
         connection.close()
+
+
+def make_backup(database_path: Path) -> None:
+    timestamp = datetime.now().strftime("%Y-%m-%dT%H-%M-%S")
+    copy2(database_path, database_path.with_suffix(f".db.{timestamp}.backup"))
 
 
 def update_datetimes(connection: sqlite3.Connection) -> None:
